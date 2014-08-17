bc85a0439695cefcddfd83c4d858b76eb211cecf
TetsuyaNegishi <yagamiraito1009@gmail.com>
2014-08-17 13:43:53 +0900

    Button TextView EditText sample

--- src/com/example/application1/MainActivity.java
+++ src/com/example/application1/MainActivity.java
@@ -9,31 +9,51 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	Context context;
+	
+	TextView textView;
+	EditText editText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		context = this;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button button = (Button) findViewById(R.id.button1);
-		button.setOnClickListener(new OnClickListener() {			
+		Button button1 = (Button) findViewById(R.id.button1);
+		button1.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
 				Toast.makeText(getApplicationContext(), "Touch", Toast.LENGTH_SHORT).show();
 			}
 		});
-		
+
 		Log.v("TAG", "v");
 		Log.d("TAG", "d");
 		Log.i("TAG", "i");
 		Log.w("TAG", "w");
 		Log.e("TAG", "e");
 		
+		
+		textView = (TextView) findViewById(R.id.textView2);
+		Button button2 = (Button) findViewById(R.id.button2);
+		editText = (EditText) findViewById(R.id.editText1);
+		
+		button2.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				String string = editText.getText().toString();
+				textView.setText(string);
+			}
+		});
+		
+		 
 	}
 
 	@Override
